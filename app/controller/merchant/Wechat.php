3bed191ef3a4b1a203ec7c4e2ba636415413a6c2
tianbo <543457309@qq.com>
2019-12-31 18:43:22 +0800

    测试

--- app/controller/merchant/Wechat.php
+++ app/controller/merchant/Wechat.php
@@ -17,7 +17,7 @@ class Wechat extends BaseController
         $callback = input('callback', "http://wap.xiaopaopao.cn");
         $callback = urlencode($callback);
 
-        $callback = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx58c160d66b6cfc41&redirect_uri={$callback}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
+        $callback = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx0c57b89185360c3b&redirect_uri={$callback}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
         
         return result(0, [
             'url' => $callback
