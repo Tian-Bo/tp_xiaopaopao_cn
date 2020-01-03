ed5e6ae779a62b84d50ad36b8c953b3514c71d46
tianbo <543457309@qq.com>
2020-01-03 19:24:51 +0800

    测试

--- route/app.php
+++ route/app.php
@@ -30,8 +30,9 @@ Route::group('wap', function () {
     });
 
 })
-->header('Access-Control-Allow-Origin', '*')
-->header('Access-Control-Allow-Headers', 'access-token, content-type')
-->header('Access-Control-Expose-Headers', 'access-token, content-type')
+->header('Access-Control-Allow-Origin', '*')    
+->header('Access-Control-Allow-Methods', '*')
+->header('Access-Control-Allow-Headers', '*')
+->header('Access-Control-Allow-Credentials', 'false')
 ->allowCrossDomain();
 
