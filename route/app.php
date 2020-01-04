aa1d4e5c14eb0d3e724bc42ff3654b5c01163018
tianbo <543457309@qq.com>
2020-01-04 23:11:27 +0800

    测试

--- route/app.php
+++ route/app.php
@@ -32,7 +32,6 @@ Route::group('wap', function () {
 })
 ->header('Access-Control-Allow-Origin', '*')    
 ->header('Access-Control-Allow-Methods', '*')
-->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-token')
-->header('Access-Control-Allow-Credentials', 'false')
+->header('Access-Control-Allow-Headers', '*')
 ->allowCrossDomain();
 
